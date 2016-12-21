ff11a5b4259bd2ce9c9720a2422468239304a845
Luis Oliva <loliva@MacBook-Pro-de-Luis.local>
2016-12-21 14:50:09 -0300

    Autoinstall docker

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-####INSTALL DOCKER UBUNTU 16.04
+#####INSTALL DOCKER UBUNTU 16.04
 
 #Actualiza y agrega key
 apt-get update && apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
