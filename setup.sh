22a9a66a396836c03d6868bae12488348bc703a9
Luis Oliva <loliva@MacBook-Pro-de-Luis.local>
2016-12-21 15:54:19 -0300

    Autoinstall docker + docker-compose and build web

--- setup.sh
+++ setup.sh
@@ -9,3 +9,10 @@ apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 
 #Actualiza 
 apt-get update && apt-get install --allow-unauthenticated -y docker-engine && systemctl status docker
+
+#Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#Build docker web
+cd /home/vagrant/data && docker-compose up web
